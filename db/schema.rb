ad8d812ec0af6a09596b3d1759f9fbea317d581e
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-03-03 23:24:51 +0100

    Created Monument model (#20)

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_201011) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_114547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "monuments", force: :cascade do |t|
+    t.string "name"
+    t.float "lat"
+    t.float "lng"
+    t.string "description"
+    t.date "completion_date"
+    t.string "location"
+    t.string "style"
+    t.string "website_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
