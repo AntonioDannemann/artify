d68563a4b2e2357768d7b52b020c65ae16cd1b64
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-04 11:24:48 +0000

    homepage blanck with camera button, with js controller working

--- app/javascript/controllers/camera_controller.js
+++ app/javascript/controllers/camera_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="camera"
 export default class extends Controller {
-  connect() {
+  openCamera() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+
+        .then(stream => {
+        })
+
+        .catch(error => {
+          console.error(error)
+        })
+    }
   }
 }
