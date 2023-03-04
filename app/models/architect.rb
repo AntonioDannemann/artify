1c37e03c55d8554af3882bc92a93b560677d6222
Louis Ramos <louisramosdev@gmail.com>
2023-03-04 11:55:11 +0100

    Added has_many through associations

--- app/models/architect.rb
+++ app/models/architect.rb
@@ -1,5 +1,6 @@
 class Architect < ApplicationRecord
   has_many :architect_monuments, dependent: :destroy
+  has_many :monuments, through: :architect_monuments
   has_one_attached :photo
 
   validates :name, :birth_date, :death_date, :nationality, :description, :photo, presence: true
