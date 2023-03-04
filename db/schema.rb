7f63f64dc070144c6a2b8a2d70aef2f2952acf3b
mariana carlota <marianacc99@hotmail.com>
2023-03-04 13:53:30 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -75,8 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_095201) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.float "longitude"
-    t.float "latitude"
+    t.float "lng"
+    t.float "lat"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
