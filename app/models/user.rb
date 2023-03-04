8fd3038174cbbea5bb7f8f20695caf910a9da154
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-03-04 18:48:19 +0100

    Created history model and updated user and monument associations (#29)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :histories, dependent: :destroy
   has_one_attached :photo
 
   validates :first_name, :last_name, :lat, :lng, presence: true
