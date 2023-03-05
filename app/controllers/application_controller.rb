b3416d6e68b506a991309cadad423eebd24979d7
mariana carlota <marianacc99@hotmail.com>
2023-03-05 10:42:19 +0000

    added photo to the sign up permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit::Authorization
 
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name lat lng])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name lat lng photo])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name lat lng photo])
   end
 
