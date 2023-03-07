598b2b54b1d5444cd19f6e393ce4948b88ba1620
Louis Ramos <louisramosdev@gmail.com>
2023-03-07 13:40:01 +0100

    Added test form to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[home]
 
-  def home() end
+  def home
+    @history = History.new
+  end
 end
