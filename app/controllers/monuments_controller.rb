88c1ca2bd2e08d2290324363c583758894a0fb83
rutger schoone <rcschoone@gmail.com>
2023-03-07 17:29:22 +0100

    re added pundit

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -3,9 +3,11 @@ class MonumentsController < ApplicationController
 
   def index
     @monuments = Monument.near([user.lat, user.lng], 5)
+    @monuments = policy_scope(Monument)
   end
 
   def show
     @monument = Monument.find(params[:id])
+    authorize @monument
   end
 end
