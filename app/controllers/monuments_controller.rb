841dab9a1a207355ed8800cbb6fda259f26de8aa
rutger schoone <rcschoone@gmail.com>
2023-03-07 17:20:02 +0100

    deleted some stuff

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -2,12 +2,7 @@ class MonumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    if params[:search].present?
-      @monuments = Monuments.global_search(params[:search])
-    else
       @monuments = Monument.near([user.lat, user.lng], 5)
-    end
-    @monuments = policy_scope(Monument)
   end
 
   def show
