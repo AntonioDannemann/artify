ed9c44a8d63ff84026643e5dac20be036cd40562
rutger schoone <rcschoone@gmail.com>
2023-03-07 15:38:10 +0100

    added index to monuments-controller

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -2,6 +2,10 @@ class MonumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def index
-
+    if params[:search].present?
+      @monuments = Monuments.global_search(params[:search])
+    else
+      @monuments = Monument.near([user.lat, user.lng], 5)
+    end
   end
 end
