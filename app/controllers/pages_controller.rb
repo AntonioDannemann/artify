f03c329dc82fa5a83fff20afe4ed47230da2a44e
Louis Ramos <louisramosdev@gmail.com>
2023-03-07 13:43:10 +0100

    Created temporary error page for History create rediration of failed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home error]
 
   def home
     @history = History.new
   end
+
+  def error() end
 end
