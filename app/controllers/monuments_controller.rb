fa101a4cc40a491b6b75875b0a9b2b82be0d4cbc
rutger schoone <rcschoone@gmail.com>
2023-03-08 21:00:04 +0100

    view is working, alteration that's need to be reverted back later

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -2,7 +2,7 @@ class MonumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @monuments = policy_scope(Monument).near([user.lat, user.lng], 5)
+    @monuments = policy_scope(Monument).near([48.858093, 2.294694], 5)
   end
 
   def show
