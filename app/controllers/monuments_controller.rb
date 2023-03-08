7b677cf1f2fa48f99e0beefd64c57539f3ddaa2d
rutger schoone <rcschoone@gmail.com>
2023-03-08 12:38:12 +0100

    skip authentication

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -1,5 +1,5 @@
 class MonumentsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @monuments = policy_scope(Monument).near([user.lat, user.lng], 5)
