c27f9dca3392220f89efba27264205362b5e2f39
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-03-13 00:27:46 +0100

    Navbar upgrade (#62)
    * Scrolling too slow on mobile, increased it a bit
    
    * Toggle navbar on scroll and move application.html script inside controller
    
    * Removed useless change from banner_scroller js controller
    
    * Changed navbar height to 55px

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,14 +13,17 @@ application.register("banner-scroller", BannerScrollerController)
 import GeolocationController from "./geolocation_controller"
 application.register("geolocation", GeolocationController)
 
-import MapController from "./map_controller"
-application.register("map", MapController)
-
 import HomeLogoController from "./home_logo_controller"
 application.register("home-logo", HomeLogoController)
+
+import MapController from "./map_controller"
+application.register("map", MapController)
 
 import ScrollToFeaturedController from "./scroll_to_featured_controller"
 application.register("scroll-to-featured", ScrollToFeaturedController)
 
 import TutorialController from "./tutorial_controller"
 application.register("tutorial", TutorialController)
+
+import WindowEventsController from "./window_events_controller"
+application.register("window-events", WindowEventsController)
