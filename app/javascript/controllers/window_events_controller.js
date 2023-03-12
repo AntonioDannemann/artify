da7168c98b7c5c1ce4eb37b91f2fe10a67e28792
Louis Ramos <louisramosdev@gmail.com>
2023-03-12 15:52:32 +0100

    Toggle navbar on scroll and move application.html script inside controller

---
+++ app/javascript/controllers/window_events_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="window-events"
+export default class extends Controller {
+  static targets = ["navbar"]
+
+  initialize() {
+    this.#scrollToTopOnReload()
+    this.#toggleNavbar()
+  }
+
+  #scrollToTopOnReload() {
+    if (history.scrollRestoration) {
+      history.scrollRestoration = 'manual';
+    } else {
+        window.onbeforeunload = () => {
+            window.scrollTo(0, 0);
+        }
+    }
+  }
+
+  #toggleNavbar() {
+    let lastScrollTop = 0
+
+    console.log(lastScrollTop)
+
+    window.addEventListener("scroll", () => {
+      const scrollTop = window.pageYOffset
+      if (scrollTop > lastScrollTop) {
+        this.navbarTarget.style.maxHeight = "0px"
+        this.navbarTarget.style.opacity = 0
+        this.navbarTarget.style.padding = "0px 20px"
+      } else if (scrollTop < lastScrollTop) {
+        this.navbarTarget.style.maxHeight = "75px"
+        this.navbarTarget.style.opacity = 1
+        this.navbarTarget.style.padding = "20px"
+      }
+      lastScrollTop = scrollTop
+    })
+  }
+}
