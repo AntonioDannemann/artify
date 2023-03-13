b30201e2d9474a8eca5e09c3922342dd7890acb1
MarianaCarlota <114437310+MarianaCarlota@users.noreply.github.com>
2023-03-13 00:02:03 +0000

    devise forms first draft (#54)
    * devise forms first draft
    
    * changed photo field
    
    * added photo avatar to edit page and password visibility option for the user
    
    * Update new.html.erb
    
    * changed controller name and targets and form.scss file
    
    * icon for hidden password

---
+++ app/javascript/controllers/toggle_hidden_password_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["button", "input"];
+
+  connect() {
+  }
+
+  toggleHiddenPassword() {
+
+
+    this.inputTarget.type = this.buttonTarget.classList.contains("fa-eye") ? "text" : "password"
+
+    this.buttonTarget.classList.toggle("fa-eye")
+    this.buttonTarget.classList.toggle("fa-eye-slash")
+  }
+}
