37f8ccb08a9dc1eae8eca51a39b3f82766ba6838
Louis Ramos <louisramosdev@gmail.com>
2023-03-14 12:16:19 +0100

    Moved monument methods from histories controller to Monument model

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -11,4 +11,30 @@ class Monument < ApplicationRecord
   def self.with_missing_photo
     Monument.all.reject { |monument| monument.photo.attached? }
   end
+
+  def attach_photo(photo_url)
+    photo = URI.parse(photo_url).open
+    if photo.size > 26_214_400
+      photo = compress_photo(photo, 40)
+    elsif photo.size > 5_242_880
+      photo = compress_photo(photo, 80)
+    end
+
+    self.photo.attach(io: photo, filename: "#{name}.jpeg", content_type: "image/jpeg")
+  end
+
+  def fetch_geocoder
+    geocoder = Geocoder.search("#{lat},#{lng}").first
+    self.city = geocoder.city
+    self.country = geocoder.country
+    self.country_code = geocoder.country_code.upcase
+  end
+
+  private
+
+  def compress_photo(photo, quality)
+    image = MiniMagick::Image.new(photo.path)
+    image.combine_options { |option| option.quality quality }
+    StringIO.open(image.to_blob)
+  end
 end
