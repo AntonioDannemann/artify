254016292875248f84a359562351234c8414792e
Louis Ramos <louisramosdev@gmail.com>
2023-03-14 12:15:49 +0100

    removed validation for photo and added class method to return Monuments without photo

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -5,6 +5,10 @@ class Monument < ApplicationRecord
   reverse_geocoded_by :lat, :lng, address: :location
   after_validation :reverse_geocode
 
-  validates :name, :description, :lat, :lng, :city, :country, :country_code, :photo, presence: true
+  validates :name, :description, :lat, :lng, :city, :country, :country_code, presence: true
   validates :lat, :lng, numericality: { only_float: true }
+
+  def self.with_missing_photo
+    Monument.all.reject { |monument| monument.photo.attached? }
+  end
 end
