b26947d53ce85d1126895260659f04de3d2341e4
Louis Ramos <louisramosdev@gmail.com>
2023-03-17 23:05:08 +0100

    Changed monument selection to be different each day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,25 @@ class PagesController < ApplicationController
 
   def home
     @history = History.new
-
-    @monuments = Monument.all
-    @monument = @monuments.sample
+    @featured_monument = featured_monument
 
     @ht = true if params[:ht]
 
-    search_monuments
+    search_form_results
   end
 
   def error() end
 
   private
 
-  def search_monuments
+  def featured_monument
+    monuments = Monument.all
+    current_unix_day = Time.current.to_time.to_i.fdiv(86_400).floor
+
+    monuments[current_unix_day % monuments.length]
+  end
+
+  def search_form_results
     @searched_monuments = []
     if params[:search] && params[:search] != ""
       @searched_monuments = @monuments.where("name ILIKE ?", "%#{params[:search]}%")
