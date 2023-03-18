f9a654500e8789ea1317ff15ca8bcd1d268189d8
Rutger Schoone <112162648+Glod33@users.noreply.github.com>
2023-03-18 12:45:51 +0100

    index redone (#81)
    * index redone
    
    * alterations for demo and button
    
    * changes
    
    * bugfixes and added logo to file
    
    ---------
    
    Co-authored-by: Louis Ramos <75388869+wJoenn@users.noreply.github.com>

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,31 +13,38 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/navigation-night-v1",
-      zoom: -20
+      zoom: 3,
     })
 
-    this.userMarker = new mapboxgl.Marker()
+    this.map.flyTo({
+      center: [2.294351, 48.858461],
+      essential: true,
+      zoom: 12
+    })
+
+    this.userMarker = new mapboxgl.Marker({ "color" : "#aa3232" })
 
     this.#addMarkersToMap()
-    navigator.geolocation.watchPosition(this.#flyMapToUser)
+    // navigator.geolocation.watchPosition(this.#flyMapToUser)
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach(marker => {
-      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map)
+      new mapboxgl.Marker({ "color": "#9ab495" }).setLngLat([marker.lng, marker.lat]).addTo(this.map)
     })
   }
 
-  #flyMapToUser = location => {
-    const latlng = [location.coords.longitude, location.coords.latitude]
+//   #flyMapToUser = location => {
+//     const latlng = [location.coords.longitude, location.coords.latitude]
 
-    this.userMarker.remove()
-    this.userMarker.setLngLat(latlng).addTo(this.map)
+//     this.userMarker.remove()
+//     this.userMarker.setLngLat(latlng).addTo(this.map)
 
-    this.map.flyTo({
-      center: latlng,
-      essential: true,
-      zoom: 13
-    })
-  }
+//     this.map.flyTo({
+//       center: [2.294351, 48.858461],
+//       essential: true,
+//       zoom: 12
+//     })
+//   }
+
 }
