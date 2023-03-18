cbb02169b9419c780dd7cee1b219440a765e28fd
Louis Ramos <louisramosdev@gmail.com>
2023-03-18 11:00:47 +0100

    Change order of camera form actions

--- app/javascript/controllers/camera_form_controller.js
+++ app/javascript/controllers/camera_form_controller.js
@@ -3,11 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="camera-form"
 export default class extends Controller {
   submit() {
-    this.element.submit()
     document.getElementById("loading").style.display = "flex"
 
     setTimeout(() => {
       document.getElementById("loading").classList.remove("hidden")
     }, 100)
+
+    this.element.submit()
   }
 }
