dc2671be9e39fb9ed7573c403445870fab1af47f
mariana carlota <marianacc99@hotmail.com>
2023-03-18 10:33:31 +0000

    display photo on sign_up form

--- app/javascript/controllers/upload_photo_controller.js
+++ app/javascript/controllers/upload_photo_controller.js
@@ -2,30 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="photo-upload"
 export default class extends Controller {
-  static targets = ["file", "icon"]
+  static targets = ["label"]
 
-  connect() {
-    const children = [...this.fileTarget.children]
-    this.first = children.find(e => e.classList.contains("selected-file"))
-    this.picture()
-  }
-
-  addFile(event) {
-    const originalInput = event.target
-    const originalParent = originalInput.parentNode
-    const file = [...originalInput.file]
-
-    this.#showPicture(file, originalInput)
-
-    const newInput = originalInput.cloneNode()
-    newInput.value = ""
-    originalParent.append(newInput)
-  }
-
-  #showPicture(event) {
-    const originalInput = event.target
-    const url = URL.createObjectURL(originalInput.file)
-    selectedFile.style.cssText = `background-image: url(${url});`
-    selectedFile.append(originalInput)
+  showPicture(event) {
+    const file = event.target.files[0]
+    const url = URL.createObjectURL(file)
+    this.labelTarget.style.backgroundImage = `url(${url})`
   }
 }
