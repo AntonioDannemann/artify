9a95a71d1eee86986ef79ea81d16e13f0921d756
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-20 21:57:37 +0000

    added transiction effect in scss and change px height to %

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -14,13 +14,17 @@ export default class extends Controller {
       this.glassContainerTarget.style.height = '86%';
       this.glassContainerTarget.style.width = '100%';
       this.descriptionTarget.style.overflow = 'scroll';
-      this.descriptionTarget.style.height = '480px';
+      this.descriptionTarget.style.height = '63%';
+      this.seeMoreButtonTarget.style.position = 'relative';
+      this.seeMoreButtonTarget.style.bottom = '6%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See Less';
     } else {
-      this.glassContainerTarget.style.height = '350px';
+      this.glassContainerTarget.style.height = '47%';
       this.glassContainerTarget.style.width = '90%';
       this.descriptionTarget.style.overflow = 'hidden';
-      this.descriptionTarget.style.height = '86px';
+      this.descriptionTarget.style.height = '30%';
+      this.seeMoreButtonTarget.style.position = 'relative';
+      this.seeMoreButtonTarget.style.bottom = '10%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See More';
     }
     this.expanded = !this.expanded;
