16fe9a1b2b8db682df846e35fededbe5f6089e3e
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-20 23:41:39 +0000

    style changes to scss

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -11,20 +11,30 @@ export default class extends Controller {
 
   toggle() {
     if (!this.expanded) {
+      // Glass container
       this.glassContainerTarget.style.height = '86%';
       this.glassContainerTarget.style.width = '100%';
+      // Description container
+      this.descriptionTarget.style.position = 'relative';
+      this.descriptionTarget.style.bottom = '6%';
       this.descriptionTarget.style.overflow = 'scroll';
-      this.descriptionTarget.style.height = '63%';
+      this.descriptionTarget.style.height = '70%';
+      // Button container
       this.seeMoreButtonTarget.style.position = 'relative';
       this.seeMoreButtonTarget.style.bottom = '6%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See Less';
     } else {
-      this.glassContainerTarget.style.height = '47%';
+      // Glass container
+      this.glassContainerTarget.style.height = '38%';
       this.glassContainerTarget.style.width = '90%';
+      // Description container
+      this.descriptionTarget.style.position = 'relative';
+      this.descriptionTarget.style.bottom = '15%';
       this.descriptionTarget.style.overflow = 'hidden';
       this.descriptionTarget.style.height = '30%';
+      // Button container
       this.seeMoreButtonTarget.style.position = 'relative';
-      this.seeMoreButtonTarget.style.bottom = '10%';
+      this.seeMoreButtonTarget.style.bottom = '15%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See More';
     }
     this.expanded = !this.expanded;
