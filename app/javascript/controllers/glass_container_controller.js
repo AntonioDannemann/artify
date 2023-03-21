28abb73d74cffe79c7d98966ba9d090625d32d63
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-21 10:51:16 +0000

    change heigh and padding when glass-container is expanded

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="glass-container"
 export default class extends Controller {
-  static targets = [ "seeMoreButton", "glassContainer", "description" ];
+  static targets = [ "seeMoreButton", "glassContainer", "description", "image" ];
   expanded = false;
 
   connect() {
@@ -12,30 +12,34 @@ export default class extends Controller {
   toggle() {
     if (!this.expanded) {
       // Glass container
-      this.glassContainerTarget.style.height = '86%';
+      this.glassContainerTarget.style.height = '90%';
       this.glassContainerTarget.style.width = '100%';
       // Description container
       this.descriptionTarget.style.position = 'relative';
-      this.descriptionTarget.style.bottom = '6%';
+      this.descriptionTarget.style.bottom = '-6%';
       this.descriptionTarget.style.overflow = 'scroll';
-      this.descriptionTarget.style.height = '70%';
+      this.descriptionTarget.style.height = '66%';
       // Button container
       this.seeMoreButtonTarget.style.position = 'relative';
-      this.seeMoreButtonTarget.style.bottom = '6%';
+      this.seeMoreButtonTarget.style.bottom = '-5%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See Less';
+      // Show the image
+      this.imageTarget.style.display = 'block';
     } else {
       // Glass container
-      this.glassContainerTarget.style.height = '38%';
+      this.glassContainerTarget.style.height = '35%';
       this.glassContainerTarget.style.width = '90%';
       // Description container
       this.descriptionTarget.style.position = 'relative';
-      this.descriptionTarget.style.bottom = '15%';
+      this.descriptionTarget.style.bottom = '13%';
       this.descriptionTarget.style.overflow = 'hidden';
       this.descriptionTarget.style.height = '30%';
       // Button container
       this.seeMoreButtonTarget.style.position = 'relative';
-      this.seeMoreButtonTarget.style.bottom = '15%';
+      this.seeMoreButtonTarget.style.bottom = '11%';
       this.seeMoreButtonTarget.querySelector('p').textContent = 'See More';
+      // Hide the image
+      this.imageTarget.style.display = 'none';
     }
     this.expanded = !this.expanded;
   }
