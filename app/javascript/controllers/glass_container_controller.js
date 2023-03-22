5fdf4f2c5903fa2f115a993faaa3bb2227639e8d
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-22 08:26:11 +0000

    change html divs, change scss added a class expanded, completly changed the glass-container stimulus controller, now more dry and clean

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -2,48 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="glass-container"
 export default class extends Controller {
-  static targets = [ "seeMoreButton", "glassContainer", "description", "image" ];
-  expanded = false;
-
-  connect() {
-    this.seeMoreButtonTarget.addEventListener('click', () => this.toggle());
+  static targets = ["glass", "content"]
+  expand(){
+    if (this.glassTarget.classList.contains("expanded")) {
+      this.glassTarget.classList.remove("expanded")
+      this.contentTarget.scrollTo({top: 0, behavior: 'smooth'})
+    } else {
+      this.glassTarget.classList.add("expanded")
+    }
   }
 
-  toggle() {
-    if (!this.expanded) {
-      // Glass container
-      this.glassContainerTarget.style.position = 'fixed';
-      this.glassContainerTarget.style.bottom = '0%';
-      this.glassContainerTarget.style.height = '90%';
-      this.glassContainerTarget.style.width = '100%';
-      this.glassContainerTarget.style.overflow = 'hidden';
-      // Description container
-      this.descriptionTarget.style.position = 'relative';
-      this.descriptionTarget.style.bottom = '30%';
-      this.descriptionTarget.style.overflow = 'scroll';
-      this.descriptionTarget.style.height = '43%';
-      // Button container
-      this.seeMoreButtonTarget.style.position = 'relative';
-      this.seeMoreButtonTarget.style.bottom = '0%';
-      this.seeMoreButtonTarget.querySelector('p').textContent = 'See Less';
-      // Show the image
-      this.imageTarget.style.display = 'block';
-    } else {
-      // Glass container
-      this.glassContainerTarget.style.height = '35%';
-      this.glassContainerTarget.style.width = '90%';
-      // Description container
-      this.descriptionTarget.style.position = 'relative';
-      this.descriptionTarget.style.bottom = '11%';
-      this.descriptionTarget.style.overflow = 'hidden';
-      this.descriptionTarget.style.height = '34%';
-      // Button container
-      this.seeMoreButtonTarget.style.position = 'relative';
-      this.seeMoreButtonTarget.style.bottom = '11%';
-      this.seeMoreButtonTarget.querySelector('p').textContent = 'See More';
-      // Hide the image
-      this.imageTarget.style.display = 'none';
+  hide(event) {
+    console.log(event.target);
+    console.log(event.target === this.element);
+    if (event.target === this.element) {
+      this.contentTarget.scrollTo({top: 0, behavior: 'smooth'})
+      this.glassTarget.classList.remove("expanded")
     }
-    this.expanded = !this.expanded;
   }
 }
