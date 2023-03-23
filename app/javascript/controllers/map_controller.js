5e745c41860dbfb4da66fbdfc34a69e9a217a680
rutger schoone <rcschoone@gmail.com>
2023-03-23 12:15:29 +0100

    commit for perfomance check

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,9 +9,12 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
+    const blinder = document.getElementById('blinder')
+    const featureListing = document.getElementById('feature-listing')
     const geojson = JSON.stringify(this.markersValue[0]);
     const features = this.markersValue[0].features
+
+    navigator.geolocation.getCurrentPosition(this.#flyMapToUser);
 
     this.map = new mapboxgl.Map({
       container: this.element,
@@ -19,50 +22,55 @@ export default class extends Controller {
       zoom: -5,
     })
 
-    navigator.geolocation.getCurrentPosition(this.#flyMapToUser);
+
 
     this.map.on('load', () => {
-      this.map.addSource("monuments", {
-        type: "geojson",
-        data: JSON.parse(geojson)
+      this.map.loadImage(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
+        (error, image) => {
+        if (error) throw error;
+
+        // Add the image to the map style.
+        this.map.addImage('cat', image);
+
+        this.map.addSource("monuments", {
+          type: "geojson",
+          data: JSON.parse(geojson)
+        });
+        this.map.addLayer({
+          'id': 'monument',
+          'source': 'monuments',
+          'type': 'symbol',
+          'layout': {
+            'icon-image': 'cat',
+            'icon-size': 0.25
+            }
+        });
       });
-      this.map.addLayer({
-        'id': 'monument',
-        'source': 'monuments',
-        'type': 'circle',
-        'paint': {
-        'circle-color': '#4264fb',
-        'circle-radius': 4,
-        },
-      });
+    })
+
+    this.map.on('click', 'monument', (e) => {
+      blinder.style.bottom = '30.2%';
+      blinder.style.backgroundColor =   'rgba(154, 180, 149, 1)';
+      featureListing.style.height = '32%';
+      featureListing.style.bottom = '0%';
+      const mon = e.features[0].properties;
+      featureListing.innerHTML = ""
+         const indexCard = document.createElement('a')
+         indexCard.href = `monuments/${mon.id}`
+         indexCard.className = 'card-monument shadow-monument';
+         indexCard.textContent = `${mon.name}`;
+         indexCard.style.backgroundImage = `url('${mon.photo}')`
+         featureListing.appendChild(indexCard);
     });
 
-    for (const feature of features) {
+    blinder.addEventListener('click', (e) => {
+      blinder.style.bottom = '-.5%';
+      blinder.style.backgroundColor =   'rgba(154, 180, 149, 0)';
+      featureListing.style.height = '0%';
+      featureListing.style.bottom = '-5%'
+    })
 
-      const el = document.createElement('a');
-      el.href = `monuments/${feature.properties.id}`
-      el.className = 'marker';
-      el.style.backgroundImage = `url('${feature.properties.photo}')`;
-
-      new mapboxgl.Marker(el)
-      .setLngLat(feature.geometry.coordinates)
-      .addTo(this.map);
-    }
-
-    this.map.on('moveend', () => {
-      const visible = this.map.queryRenderedFeatures({ layers: ['monument'] });
-      if (visible.length) {
-        this.#renderListings(visible)
-      }else {
-         const listingEl = document.getElementById('feature-listing')
-         listingEl.innerHTML = ""
-         const emptyItem = document.createElement('div')
-         emptyItem.className = 'card-monument shadow-monument';
-         emptyItem.textContent = 'scroll to find monuments';
-         emptyItem.style.backgroundImage = `url('https://newfoundtrees.org/static/media/doge.3636fa73.jpg')`
-         listingEl.appendChild(emptyItem);
-      }
-    });
 
     this.map.addControl(
       new mapboxgl.GeolocateControl({
@@ -75,6 +83,37 @@ export default class extends Controller {
       showUserHeading: true
       })
       );
+
+    // for (const feature of features) {
+
+    //   const el = document.createElement('div');
+    //   el.className = 'marker';
+    //   el.style.backgroundImage = `url('https://cdn-icons-png.flaticon.com/512/263/263429.png')`;
+
+    //   const popup = new mapboxgl.Popup({ closeButton: false })
+    //   popup.setText(feature.properties.name);
+
+    //   new mapboxgl.Marker(el)
+    //   .setLngLat(feature.geometry.coordinates)
+    //   .setPopup(popup)
+    //   .addTo(this.map);
+    // }
+
+    // this.map.on('moveend', () => {
+    //   const visible = this.map.queryRenderedFeatures({ layers: ['monument'] });
+    //   if (visible.length) {
+    //     this.#renderListings(visible)
+    //   }else {
+    //      const listingEl = document.getElementById('feature-listing')
+        //  listingEl.innerHTML = ""
+        //  const emptyItem = document.createElement('div')
+        //  emptyItem.className = 'card-monument shadow-monument';
+        //  emptyItem.textContent = 'scroll to find monuments';
+        //  emptyItem.style.backgroundImage = `url('https://newfoundtrees.org/static/media/doge.3636fa73.jpg')`
+        //  listingEl.appendChild(emptyItem);
+    //   }
+    // });
+
   }
 
   #flyMapToUser = position =>  {
@@ -85,19 +124,19 @@ export default class extends Controller {
     })
   }
 
-  #renderListings(monuments) {
-    const listingEl = document.getElementById('feature-listing');
-    listingEl.innerHTML = '';
+  // #renderListings(monuments) {
+  //   const listingEl = document.getElementById('feature-listing');
+  //   listingEl.innerHTML = '';
 
-    if (monuments.length) {
-      for (const monument of monuments) {
-        const itemLink = document.createElement('a');
-        itemLink.className = 'card-monument shadow-monument';
-        itemLink.href = `monuments/${monument.properties.id}`;
-        itemLink.textContent = monument.properties.name;
-        itemLink.style.backgroundImage = `url('${monument.properties.photo}')`
-        listingEl.appendChild(itemLink);
-      }
-    }
-  }
+  //   if (monuments.length) {
+  //     for (const monument of monuments) {
+  //       const itemLink = document.createElement('a');
+  //       itemLink.className = 'card-monument shadow-monument';
+  //       itemLink.href = `monuments/${monument.properties.id}`;
+  //       itemLink.textContent = monument.properties.name;
+  //       itemLink.style.backgroundImage = `url('${monument.properties.photo}')`
+  //       listingEl.appendChild(itemLink);
+  //     }
+  //   }
+  // }
 }
