e96e4db043ccc12ba9f15819766747b2b541cf76
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-23 22:55:31 +0000

    added truncate affect tho the desription text, and methdo to remove it in the glass-controler stimulus controller

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -1,23 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="glass-container"
 export default class extends Controller {
-  static targets = ["glass", "content"]
+  static targets = ["glass", "content", "truncate"]
+
   expand(){
     if (this.glassTarget.classList.contains("expanded")) {
       this.glassTarget.classList.remove("expanded")
       this.contentTarget.scrollTo({top: 0, behavior: 'smooth'})
+      this.truncateTargets.forEach(el => el.classList.add("truncate"))
     } else {
       this.glassTarget.classList.add("expanded")
+      this.truncateTargets.forEach(el => el.classList.remove("truncate"))
     }
   }
 
   hide(event) {
-    console.log(event.target);
-    console.log(event.target === this.element);
     if (event.target === this.element) {
       this.contentTarget.scrollTo({top: 0, behavior: 'smooth'})
       this.glassTarget.classList.remove("expanded")
+      this.truncateTargets.forEach(el => el.classList.add("truncate"))
     }
   }
 }
