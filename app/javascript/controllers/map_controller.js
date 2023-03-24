6af37e7792ac2a2dde05accaa2ca09c19120a7e5
rutger schoone <rcschoone@gmail.com>
2023-03-24 12:39:43 +0100

    ready for review

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     const blinder = document.getElementById('blinder')
     const featureListing = document.getElementById('feature-listing')
     const geojson = JSON.stringify(this.markersValue[0]);
-    const features = this.markersValue[0].features
+    // const features = this.markersValue[0].features
 
     navigator.geolocation.getCurrentPosition(this.#flyMapToUser);
 
@@ -30,7 +30,6 @@ export default class extends Controller {
         (error, image) => {
         if (error) throw error;
 
-        // Add the image to the map style.
         this.map.addImage('pin', image);
 
         this.map.addSource("monuments", {
@@ -57,41 +56,25 @@ export default class extends Controller {
 
       featureListing.style.height = '285px';
       featureListing.style.bottom = '0%';
+      featureListing.innerHTML = ""
 
       const mon = e.features[0].properties;
-
-
-
-      featureListing.innerHTML = ""
-        // const indexImg = document.createElement('a')
-        // indexImg.href = `monuments/${mon.id}`
-        // indexImg.className = 'card-index';
-        // indexImg.style.backgroundImage = `url('${mon.photo}')`
-        // featureListing.appendChild(indexImg);
-
-        // const indexCard = document.createElement('a')
-        // indexCard.href = `monuments/${mon.id}`
-        // indexCard.className = 'card-index';
-        // indexCard.style.backgroundImage = `url('${mon.photo}')`
-        // featureListing.appendChild(indexCard);
-
-        const indexCard = document.createElement('div')
-        indexCard.innerHTML =`
-        <div class="card-index">
-          <div class="image-index" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0.7)), url('${mon.photo}')">
-          </div>
-          <div class="description-index">
-            <div>
-              <h3>${mon.name}</h3>
-              <p><i class="fa-solid fa-location-dot"></i>  ${mon.city} </p>
-            </div>
-            <p class="desc-short">${mon.desc}...</p>
-            <a href="/monuments/1"> Learn more <i class="fa-solid  fa-arrow-right"></i></a>
-          </div>
+      const indexCard = document.createElement('div')
+      indexCard.innerHTML =`
+      <div class="card-index">
+        <div class="image-index" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2) 30%, rgba(0, 0, 0, 0.7)), url('${mon.photo}')">
         </div>
-        `;
-        featureListing.appendChild(indexCard);
-
+        <div class="description-index">
+          <div>
+            <h3>${mon.name}</h3>
+            <p><i class="fa-solid fa-location-dot"></i>  ${mon.city} </p>
+          </div>
+          <p class="desc-short">${mon.desc}...</p>
+          <a href="/monuments/1"> Learn more <i class="fa-solid  fa-arrow-right"></i></a>
+        </div>
+      </div>
+      `;
+      featureListing.appendChild(indexCard);
     });
 
     blinder.addEventListener('click', (e) => {
@@ -102,6 +85,15 @@ export default class extends Controller {
       featureListing.style.height = '0%';
       featureListing.style.bottom = '-5%'
     })
+
+    this.map.on('movestart', () => {
+      blinder.style.bottom = '-.5%';
+      blinder.style.backgroundColor =   'rgba(154, 180, 149, 0)';
+
+      featureListing.style.position = 'fixed';
+      featureListing.style.height = '0%';
+      featureListing.style.bottom = '-5%'
+    });
 
 
     this.map.addControl(
@@ -115,37 +107,6 @@ export default class extends Controller {
       showUserHeading: true
       })
       );
-
-    // for (const feature of features) {
-
-    //   const el = document.createElement('div');
-    //   el.className = 'marker';
-    //   el.style.backgroundImage = `url('https://cdn-icons-png.flaticon.com/512/263/263429.png')`;
-
-    //   const popup = new mapboxgl.Popup({ closeButton: false })
-    //   popup.setText(feature.properties.name);
-
-    //   new mapboxgl.Marker(el)
-    //   .setLngLat(feature.geometry.coordinates)
-    //   .setPopup(popup)
-    //   .addTo(this.map);
-    // }
-
-    // this.map.on('moveend', () => {
-    //   const visible = this.map.queryRenderedFeatures({ layers: ['monument'] });
-    //   if (visible.length) {
-    //     this.#renderListings(visible)
-    //   }else {
-    //      const listingEl = document.getElementById('feature-listing')
-        //  listingEl.innerHTML = ""
-        //  const emptyItem = document.createElement('div')
-        //  emptyItem.className = 'card-monument shadow-monument';
-        //  emptyItem.textContent = 'scroll to find monuments';
-        //  emptyItem.style.backgroundImage = `url('https://newfoundtrees.org/static/media/doge.3636fa73.jpg')`
-        //  listingEl.appendChild(emptyItem);
-    //   }
-    // });
-
   }
 
   #flyMapToUser = position =>  {
@@ -155,20 +116,4 @@ export default class extends Controller {
       zoom: 12
     })
   }
-
-  // #renderListings(monuments) {
-  //   const listingEl = document.getElementById('feature-listing');
-  //   listingEl.innerHTML = '';
-
-  //   if (monuments.length) {
-  //     for (const monument of monuments) {
-  //       const itemLink = document.createElement('a');
-  //       itemLink.className = 'card-monument shadow-monument';
-  //       itemLink.href = `monuments/${monument.properties.id}`;
-  //       itemLink.textContent = monument.properties.name;
-  //       itemLink.style.backgroundImage = `url('${monument.properties.photo}')`
-  //       listingEl.appendChild(itemLink);
-  //     }
-  //   }
-  // }
 }
