6dac1777c07977a703c90c251229f28c2d3b262e
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-24 22:16:15 +0000

    change the view file where there was a huge code, and turn that into variable in the controler, now is more DRY, as suggested in the PR review

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -13,10 +13,8 @@ class HistoriesController < ApplicationController
     @monument = @history.monument
     @monuments = Monument.where(city: @monument.city).where.not(id: @monument.id)
     paragraphs = @monument.description.split(". ")
-    @first_paragraph = paragraphs.shift(2).join(". ")
-    @second_paragraph = @monument.description.split(". ")[2..].each_slice(3).map do |subarray|
-      "<p>" + subarray.join(". ") + ".</p>"
-    end.join("\n").html_safe
+    @first_para = paragraphs.shift(2).join(". ")
+    @second_para = @monument.description.split(". ")[2..].each_slice(3).map { |subarr| subarr.join(". ") }
     authorize @history
   end
 
--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -13,10 +13,8 @@ class MonumentsController < ApplicationController
     @monument = Monument.find(params[:id])
     @monuments = Monument.where(city: @monument.city)
     paragraphs = @monument.description.split(". ")
-    @first_paragraph = paragraphs.shift(2).join(". ")
-    @second_paragraph = @monument.description.split(". ")[2..].each_slice(3).map do |subarray|
-      "<p>" + subarray.join(". ") + ".</p>"
-    end.join("\n").html_safe
+    @first_para = paragraphs.shift(2).join(". ")
+    @second_para = @monument.description.split(". ")[2..].each_slice(3).map { |subarr| subarr.join(". ") }
     authorize @monument
   end
 end
