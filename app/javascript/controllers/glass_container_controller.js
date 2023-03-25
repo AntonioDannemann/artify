efb3fcec342c96f7b4ebcf2cd965a7d73248be41
Louis Ramos <louisramosdev@gmail.com>
2023-03-25 12:08:33 +0100

    Cleaned some html and css file

--- app/javascript/controllers/glass_container_controller.js
+++ app/javascript/controllers/glass_container_controller.js
@@ -5,15 +5,25 @@ export default class extends Controller {
 
   toggleGlass() {
     this.glassTarget.classList.toggle("expanded")
-    this.truncateTarget.classList.toggle("truncate")
     this.contentTarget.scrollTo({ top: 0, behavior: 'smooth' })
+
+    if (this.glassTarget.classList.contains("expanded")) {
+      this.truncateTarget.classList.remove("truncate")
+    } else {
+      setTimeout(() => {
+        this.truncateTarget.classList.add("truncate")
+      }, 450);
+    }
   }
 
   shrinkGlass(event) {
     if (event.target === this.element) {
       this.contentTarget.scrollTo({ top: 0, behavior: 'smooth' })
       this.glassTarget.classList.remove("expanded")
-      this.truncateTarget.classList.toggle("truncate")
+
+      setTimeout(() => {
+        this.truncateTarget.classList.add("truncate")
+      }, 450);
     }
   }
 }
