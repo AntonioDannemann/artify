bfe9f55dd47aef2b5eb5c7bb4ebc15f3dbbdf12d
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-27 00:13:55 +0100

    fixed whats was suggesed in the PR comment

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,15 +2,16 @@ class FavouritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @favourites = current_user.favourites.includes(:monument)
+    @favourites = current_user.favourites.where(user_id: current_user.id)
   end
 
   def create
-    @favourites = current_user.favorites.new(monument_id: params[:monument_id])
-    authorize @favorite
+    @monument = Monument.find(params[:monument_id])
+    @favourite = Favourite.new(user: current_user, monument: @monument)
+    authorize @favourite
 
     if @favourite.save
-      redirect_to @favorite.monument
+      redirect_to @favourite.monument
     else
       redirect_to @monument
     end
