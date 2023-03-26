08b9ab0c14218f45c0f2770d9ebe32161365539d
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-26 14:53:47 +0100

    updated the router to include the favorites

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "error", to: "pages#error", as: :error
 
   resources :histories, only: %i[index show create]
-  resources :monuments, only: %i[index show]
+  resources :monuments, only: %i[index show] do
+    resources :favorites, only: %i[create destroy]
+  end
   get "dashboard", to: "users#dashboard", as: :dashboard
 end
