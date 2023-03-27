5877a62708c0b8114fe4082d186b6a85f32b3961
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-03-27 19:02:03 +0200

    Logic for tourist and sprinter added to achievement progression (#184)

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ puts "Done creating users\n\n"
 Achievement.create!(title: "Let's begin", description: "Scan your first landmark", goal: 1, keyword: "all")
 Achievement.create!(title: "Get Going", description: "Scan 10 landmarks", goal: 10, keyword: "all")
 Achievement.create!(title: "Paris Explorer", description: "Scan 5 landmarks from Paris", goal: 5, keyword: "Paris")
+Achievement.create!(title: "Sprinter", description: "Scan 5 landmarks in less than 24 hours", goal: 5, keyword: "all")
+Achievement.create!(title: "Tourist", description: "Scan landmarks in 5 different countries", goal: 5, keyword: "all")
 
 arc_de_triomphe = "https://cdn.britannica.com/66/80466-050-2E125F5C/Arc-de-Triomphe-Paris-France.jpg"
 atomium = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Brussels_-_Atomium_2022.jpg"
