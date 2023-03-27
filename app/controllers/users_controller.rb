f7eb9ee29c116e157e6e5c4f654b04f2c0c5fdef
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-03-27 16:11:31 +0200

    Removed pundit from project (#185)
    * Remove pundit from project
    
    * redirect to root if user isn't history user
    
    * Removed pundit gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  skip_after_action :verify_authorized
-
   def dashboard
     @user = current_user
-    @histories = History.where(user: @user).order(updated_at: :desc)
+    @histories = @user.histories.order(updated_at: :desc)
   end
 end
