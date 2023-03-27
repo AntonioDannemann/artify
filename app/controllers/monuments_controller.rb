ede931bccd4f2a64d6125f1279c8d45ea3dbfafb
Rutger Schoone <112162648+Glod33@users.noreply.github.com>
2023-03-27 22:44:15 +0200

    Nearby v3 (#121)
    * images added to file
    
    * a working and improved map
    
    * first draft
    
    * added shadow to cards
    
    * link is working
    
    * map flies to user on load
    
    * tiny fix
    
    * back up commit
    
    * draft 2
    
    * before refactor
    
    * some refactoring
    
    * dropped image quality to lowest
    
    * commit for perfomance check
    
    * perfomance check before styling
    
    * changed icon to be super visible not final
    
    * before refactor
    
    * ready for review
    
    * commit before merging master
    
    * fixed most issues, added fetch, refactored code
    
    * redsigned pin
    
    * ready
    
    * all requested changes

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -3,6 +3,12 @@ class MonumentsController < ApplicationController
 
   def index
     @user = current_user
+    @monument = Monument.find_by(id: params[:id])
+    @markers = [{
+      type: "FeatureCollection",
+      features: []
+    }]
+
     @lat = 48.858461
     @lng = 2.294351
 
@@ -10,7 +16,8 @@ class MonumentsController < ApplicationController
     @nearby_monuments = @monuments.select { |mon| mon.distance_between(@lat, @lng) < 5 }
                                   .sort_by { |mon| mon.distance_between(@lat, @lng) }
 
-    @markers = @monuments.map { |monument| { lat: monument.lat, lng: monument.lng } }
+    respond
+    monument_markers
   end
 
   def show
@@ -19,5 +26,34 @@ class MonumentsController < ApplicationController
 
     @first_para = @monument.description.split(". ").first(2).join(". ")
     @second_para = @monument.description.split(". ")[2..].each_slice(3).map { |subarr| subarr.join(". ") }
+  end
+
+  private
+
+  def respond
+    respond_to do |format|
+      format.html
+
+      partial = "monuments/card_index"
+      locals = { mon: @monument }
+      format.text { render partial:, locals:, formats: [:html] }
+    end
+  end
+
+  def monument_markers
+    @monuments.each do |m|
+      @markers.first[:features].push(
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [m.lng, m.lat]
+          },
+          properties: {
+            id: m.id
+          }
+        }
+      )
+    end
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -7,40 +7,86 @@ export default class extends Controller {
     markers: Array
   }
 
+  static targets = ["map", "listing", "blinder"]
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
+    const geojson = JSON.stringify(this.markersValue[0]);
 
+    // navigator.geolocation.getCurrentPosition(this.#flyMapToUser);
     this.map = new mapboxgl.Map({
-      container: this.element,
+      container: this.mapTarget,
       style: "mapbox://styles/mapbox/navigation-night-v1",
-      zoom: 3,
+      zoom: -5,
     })
 
-    this.userMarker = new mapboxgl.Marker({ "color" : "#aa3232" })
+    navigator.geolocation.getCurrentPosition(location => {
+      this.map.flyTo({
+        center: [location.coords.longitude, location.coords.latitude, ],
+        essential: true,
+        zoom: 12
+      })
+      const lat = location.coords.latitude
+      const lng = location.coords.longitude
 
-    this.#addMarkersToMap()
-    this.#flyMapToUser()
-    navigator.geolocation.watchPosition(this.#updateUserPosition)
+      this.map.on('click', 'monument', event => {
+        this.blinderTarget.classList.add('blinder-expanded')
+        const mon = event.features[0].properties;
+        const url = `/monuments?lat=${lat}&lng=${lng}&id=${mon.id}`
+
+        fetch(url, { headers: { "Accept": "text/plain" } })
+          .then(res => res.text())
+          .then(html => {
+            this.listingTarget.innerHTML = html
+            this.listingTarget.classList.remove('listing-hidden')
+          })
+      });
+
+    });
+
+    this.map.on('load', () => {
+      this.map.loadImage(
+        "/assets/location_pin-426cf5b931d1f7c30012d7662bfd0ad73b4b72e1667f9846706332a375a04000.png",
+        // <%# <%= image_path "location-pin.png" %>
+        (error, image) => {
+        if (error) throw error;
+
+        this.map.addImage('pin', image);
+
+        this.map.addSource("monuments", {
+          type: "geojson",
+          data: JSON.parse(geojson)
+        });
+        this.map.addLayer({
+          'id': 'monument',
+          'source': 'monuments',
+          'type': 'symbol',
+          'layout': {
+            "icon-allow-overlap" : true,
+            "icon-padding" : 10,
+            'icon-image': 'pin',
+            "icon-size": ['interpolate', ['linear', 2], ['zoom'], 2, 0.04, 10, 0.025, 12, 0.04]
+            }
+        });
+      });
+    });
+
+    this.map.on('movestart', () => {
+      this.blinderTarget.classList.remove('blinder-expanded')
+      this.listingTarget.classList.add('listing-hidden')
+    });
+
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+      })
+    this.map.addControl(geolocate);
   }
 
-  #addMarkersToMap() {
-    this.markersValue.forEach(marker => {
-      new mapboxgl.Marker({ "color": "#9ab495" }).setLngLat([marker.lng, marker.lat]).addTo(this.map)
-    })
-  }
-
-  #flyMapToUser() {
-    this.map.flyTo({
-      center: [2.294351, 48.858461],
-      essential: true,
-      zoom: 12
-    })
-  }
-
-  #updateUserPosition = location => {
-    const latlng = [location.coords.longitude, location.coords.latitude]
-
-    this.userMarker.remove()
-    this.userMarker.setLngLat(latlng).addTo(this.map)
+  lower() {
+    this.blinderTarget.classList.remove('blinder-expanded')
+    this.listingTarget.classList.add('listing-hidden')
   }
 }
