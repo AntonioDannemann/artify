ff359ab63d81094c49bdd8f052c73d7c6d464448
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-03-28 16:25:58 +0100

    updated the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     @histories = @user.histories.order(updated_at: :desc)
+    @favourite_monuments = @user.favourite_monuments.order(updated_at: :desc)
   end
 end
