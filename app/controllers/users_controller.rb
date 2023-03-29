6d1c002d6b9301dcc7d2cf3d3215f01bc73871cc
mariana carlota <marianacc99@hotmail.com>
2023-03-29 23:05:06 +0100

    removed unnecessary line and added style to the flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
     @user = current_user
     @histories = @user.histories.order(updated_at: :desc)
     @achievements = @user.user_achievements.order("status DESC, updated_at DESC").first(5)
-    @completed_achievements = []
 
     completed_achievements
   end
@@ -12,9 +11,11 @@ class UsersController < ApplicationController
 
   def completed_achievements
     @completed_achievements = []
+
     @user.user_achievements.each do |ach|
       @completed_achievements << ach if ach.completed?
     end
+
     @completed_achievements = @completed_achievements.last(3)
   end
 end
