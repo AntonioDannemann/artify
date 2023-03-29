563cc05fa92220f4b9c7e13afda111d12d0a454d
Louis Ramos <louisramosdev@gmail.com>
2023-03-29 18:29:59 +0200

    Achievements index completed

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,6 +1,7 @@
 class AchievementsController < ApplicationController
   def index
     @user = current_user
-    @achievements = @user.user_achievements.order("status DESC, updated_at DESC")
+    @user_achievements = @user.user_achievements.order("status DESC, updated_at DESC")
+    @achievements = Achievement.all - @user.user_achievements.map(&:achievement)
   end
 end
