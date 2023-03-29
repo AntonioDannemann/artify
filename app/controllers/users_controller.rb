e2b4ea05cb915a99d67c12b3572d02078899ffa0
mariana carlota <marianacc99@hotmail.com>
2023-03-29 22:26:26 +0100

    completed achievements on dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
     @user = current_user
     @histories = @user.histories.order(updated_at: :desc)
     @achievements = @user.user_achievements.order("status DESC, updated_at DESC").first(5)
+    @completed_achievements = []
+
+    completed_achievements
+  end
+
+  private
+
+  def completed_achievements
+    @completed_achievements = []
+    @user.user_achievements.each do |ach|
+      @completed_achievements << ach if ach.completed?
+    end
+    @completed_achievements = @completed_achievements.last(3)
   end
 end
