6d3e74c5d7dc7390129e52b9b2d8fb39d5a6d14f
mariana carlota <marianacc99@hotmail.com>
2023-03-30 18:51:05 +0100

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,6 @@ class UsersController < ApplicationController
     @user = current_user
     @histories = @user.histories.order(updated_at: :desc)
     @achievements = @user.user_achievements.order("status DESC, updated_at DESC").first(5)
-
-    completed_achievements
-  end
-
-  private
-
-  def completed_achievements
-    @completed_achievements = []
-
-    @user.user_achievements.each do |ach|
-      @completed_achievements << ach if ach.completed?
-    end
-
-    @completed_achievements = @completed_achievements.last(3)
+    @completed_achievements = @user.user_achievements.select(&:completed?).last(3).map(&:achievement)
   end
 end
