ab43e44d1395ed941f065eb63b68902e1bfff6bf
Louis Ramos <louisramosdev@gmail.com>
2023-04-01 10:42:03 +0200

    Reversed completed badges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
     @histories = @user.histories.order(updated_at: :desc)
     @favourite_monuments = current_user.favourites.order(created_at: :desc).map(&:monument)
     @achievements = @user.user_achievements.order("status DESC, updated_at DESC").first(5)
-    @completed_achievements = @user.user_achievements.select(&:completed?).last(3).map(&:achievement)
+    @completed_achievements = @user.user_achievements.select(&:completed?).last(3).map(&:achievement).reverse
   end
 end
